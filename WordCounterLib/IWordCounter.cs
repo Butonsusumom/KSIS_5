037725ff86e057165015bfedeed57099287cf140
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2020-04-06 23:52:32 +0300

    Add files via upload

---
+++ WordCounterLib/IWordCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WordCounterLib
+{
+    [ServiceContract(Namespace = "http://wordcounter.com/")]
+    public interface IWordCounter
+    {
+        [OperationContract]
+        int CountWords(string text);
+    }
+}
